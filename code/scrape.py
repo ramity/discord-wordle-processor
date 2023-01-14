0562b20540d088db9c805820045a358e5c1fd1c6
db2 <lcb005@uark.edu>
2023-01-14 02:12:11 +0000

    fixes #6 - uses configured channel in print output + resolves an error with the previous update to use env var for channel

--- code/scrape.py
+++ code/scrape.py
@@ -8,7 +8,7 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
-        print(f'Pulling messages from #wordle textchannel.')
+        print('Pulling messages from #' + os.getenv('CHANNEL') + ' textchannel.')
 
         messages = {}
         channel = self.get_wordle_channel()
@@ -19,20 +19,20 @@ class MyClient(discord.Client):
                 messages[author] = []
             messages[author].append(message.content)
 
-        print(f'Loaded all messages.')
+        print('Loaded all messages.')
 
         with open('dump.pkl', 'wb') as f:
             pickle.dump(messages, f)
 
-        print(f'Saved all messages.')
-        print(f'Exiting.')
+        print('Saved all messages.')
+        print('Exiting.')
 
         await self.close()
 
     def get_wordle_channel(self):
         for server in self.guilds:
             for channel in server.channels:
-                if str(channel.type) == 'text' and str(channel.name) == os.getenv('CHANNEL')':
+                if str(channel.type) == 'text' and str(channel.name) == os.getenv('CHANNEL'):
                     return channel
 
 intents = discord.Intents.default()
